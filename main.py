e03dd8a2fe38e1dcc0a80f2abca4e637f9ce1253
Neel Khare <neelk2@illinois.edu>
2021-07-01 20:14:43 +0100

    Added support to change prefix

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import time
+import json
 from dotenv import load_dotenv
 from pyzoom import ZoomClient
 from datetime import datetime as dt
@@ -8,10 +9,48 @@ from discord.ext import commands
 load_dotenv()
 KEY = os.getenv('key')
 SECRET = os.getenv('secret')
+TOKEN = os.getenv('token')
 client = ZoomClient(KEY, SECRET)
 
-bot = commands.Bot(command_prefix='$', help_command=None)
-prefix = '$'
+def get_prefix(bot, message):
+    with open('prefixes.json', 'r') as f:
+        prefixes = json.load(f)
+
+    return prefixes[str(message.guild.id)]
+
+bot = commands.Bot(command_prefix = get_prefix, help_command=None)
+
+@bot.event
+async def on_guild_join(guild):
+    with open('prefixes.json', 'r') as f:
+        prefixes = json.load(f)
+
+    prefixes[str(guild.id)] = '$'
+
+    with open('prefixes.json', 'w') as f:
+        json.dump(prefixes, f, indent=4)
+
+@bot.event
+async def on_guild_remove(guild):
+    with open('prefixes.json', 'r') as f:
+        prefixes = json.load(f)
+
+    prefixes.pop(str(guild.id))
+
+    with open('prefixes.json', 'w') as f:
+        json.dump(prefixes, f, indent=4)
+
+@bot.command(name="changeprefix")
+async def change_prefix(ctx, prefix):
+    with open('prefixes.json', 'r') as f:
+        prefixes = json.load(f)
+
+    prefixes[str(ctx.guild.id)] = prefix
+
+    with open('prefixes.json', 'w') as f:
+        json.dump(prefixes, f, indent=4)
+
+    await ctx.send(f'prefix changed to: {prefix}')
 
 @bot.command(name="new")
 async def new(ctx, arg):
@@ -28,7 +67,4 @@ async def new(ctx, arg):
     if arg == "gmeet".lower():
         await ctx.send("http://meet.google.com/new")
 
-with open("TOKEN.txt", "r") as token_file:
-    TOKEN = token_file.read()
-    print('connected')
-    bot.run(TOKEN)
+bot.run(TOKEN)
